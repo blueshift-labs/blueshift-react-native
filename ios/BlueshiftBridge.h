3d6a3ced01b1933036e48aa756dc70993d5944e3
Rahul Raveendran <rahul.raveendran@getblueshift.com>
2023-03-29 13:22:04 +0530

    Release 1.0.2

--- ios/BlueshiftBridge.h
+++ ios/BlueshiftBridge.h
@@ -12,7 +12,7 @@
 NS_ASSUME_NONNULL_BEGIN
 #define kScreenViewed @"screen_viewed"
 
-#define kBlueshiftReactSDKVersion @"1.0.1"
+#define kBlueshiftReactSDKVersion @"1.0.2"
 
 @interface BlueshiftBridge : NSObject <RCTBridgeModule>
 
