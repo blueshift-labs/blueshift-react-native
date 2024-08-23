9f2fd6c13faf27e69a4d1b0e0335594b9e54ed92
Rahul Raveendran <rahul.raveendran@getblueshift.com>
2024-08-23 15:37:29 +0530

    added firebase and integrated blueshift sdk 3.5.1

--- BlueshiftSampleApp/android/app/src/main/java/com/mynewapp/MainApplication.kt
+++ BlueshiftSampleApp/android/app/src/main/java/com/mynewapp/MainApplication.kt
@@ -1,6 +1,9 @@
 package com.mynewapp
 
 import android.app.Application
+import com.blueshift.Blueshift
+import com.blueshift.BlueshiftLogger
+import com.blueshift.model.Configuration
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
@@ -14,20 +17,20 @@ import com.facebook.soloader.SoLoader
 class MainApplication : Application(), ReactApplication {
 
   override val reactNativeHost: ReactNativeHost =
-      object : DefaultReactNativeHost(this) {
-        override fun getPackages(): List<ReactPackage> =
-            PackageList(this).packages.apply {
-              // Packages that cannot be autolinked yet can be added manually here, for example:
-              // add(MyReactNativePackage())
-            }
+    object : DefaultReactNativeHost(this) {
+      override fun getPackages(): List<ReactPackage> =
+        PackageList(this).packages.apply {
+          // Packages that cannot be autolinked yet can be added manually here, for example:
+          // add(MyReactNativePackage())
+        }
 
-        override fun getJSMainModuleName(): String = "index"
+      override fun getJSMainModuleName(): String = "index"
 
-        override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
+      override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
 
-        override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
-        override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
-      }
+      override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+      override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
+    }
 
   override val reactHost: ReactHost
     get() = getDefaultReactHost(applicationContext, reactNativeHost)
@@ -39,5 +42,26 @@ class MainApplication : Application(), ReactApplication {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
       load()
     }
+    // Enable logging to view SDK logs in logcat window.
+    if (BuildConfig.DEBUG) {
+      // You must disable logging in production.
+      BlueshiftLogger.setLogLevel(BlueshiftLogger.VERBOSE);
+    }
+
+    initBlueshift()
+  }
+
+  private fun initBlueshift() {
+    val configuration = Configuration();
+    // Set Blueshift event API key
+    configuration.apiKey = BuildConfig.API_KEY;
+    // Enable in-app messages
+    configuration.isInAppEnabled = true;
+    configuration.isJavaScriptForInAppWebViewEnabled = true;
+    // Set device-id source to Instance Id and package name combo (highly recommended)
+    configuration.deviceIdSource = Blueshift.DeviceIdSource.INSTANCE_ID_PKG_NAME;
+    // Enable mobile inbox
+    configuration.isInboxEnabled = true;
+    Blueshift.getInstance(this).initialize(configuration);
   }
 }
